2b962951b403bd03bbdd6fb75569457ab6ed6c0f
Rolly93 <129896901+Rolly93@users.noreply.github.com>
2025-04-21 19:26:52 +0000

    Como crear un log handler y como almacenar dentro de un archivo 'log' y formato

--- python-course/loggin/log_handlers.py
+++ python-course/loggin/log_handlers.py
@@ -2,3 +2,28 @@ import logging
 
 
 logger = logging.getLogger(__name__)
+#definicion de su nivel
+logger.setLevel(logging.DEBUG)
+
+
+#este es para crear un handler
+handler_consola = logging.StreamHandler()
+
+#este es para crear el formato del handler
+format_logs = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+#agregar el formato
+handler_consola.setFormatter(format_logs)
+
+#definicion a donde pertenece el logger
+
+logger.addHandler(handler_consola)
+
+#nombre del archivo en el que se almacena los logs
+handle_archivo = logging.FileHandler("arvhivo.log")
+
+handle_archivo.setLevel(logging.ERROR)
+handle_archivo.setFormatter(format_logs)
+logger.addHandler(handle_archivo)
+
+logger.info("Registro informativo")
